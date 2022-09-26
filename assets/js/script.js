d1263347cbc3766da7ae407366d9c09ba059edd4
Dennis Kimathi <kimathidennis38@gmail.com>
2022-09-26 16:30:14 +0300

    update projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,40 @@
+let tabsContainer = document.querySelector("#tabs");
+
+let tabTogglers = tabsContainer.querySelectorAll("a");
+console.log(tabTogglers);
+
+tabTogglers.forEach(function (toggler) {
+  toggler.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    let tabName = this.getAttribute("href");
+
+    let tabContents = document.querySelector("#tab-contents");
+
+    for (let i = 0; i < tabContents.children.length; i++) {
+      tabTogglers[i].parentElement.classList.remove(
+        "border-[#971E39]",
+        "border-b",
+        "-mb-px",
+        "opacity-100"
+      );
+      tabContents.children[i].classList.remove("hidden");
+      if ("#" + tabContents.children[i].id === tabName) {
+        continue;
+      }
+      tabContents.children[i].classList.add("hidden");
+    }
+    e.target.parentElement.classList.add(
+      "border-[#971E39]",
+      "border-b-4",
+      "-mb-px",
+      "opacity-20"
+    );
+  });
+});
+
+document.getElementById("default-tab").click();
+
 //Smooth Scroll Effect
 
 function smoothScroll(target, duration) {
